505edb880d489385ed9dcaca9f80882b4f72c729
flying_pig <1839976096@qq.com>
2025-01-03 23:35:04 +0800

    fix: 自动注册服务器配置类

--- demo/src/main/java/com/flyingpig/demo/DemoApplication.java
+++ demo/src/main/java/com/flyingpig/demo/DemoApplication.java
@@ -5,9 +5,6 @@ import com.flyingpig.boot.annotation.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
--- pig-boot/src/main/java/com/flyingpig/boot/SpringApplication.java
+++ pig-boot/src/main/java/com/flyingpig/boot/SpringApplication.java
@@ -20,7 +20,7 @@ public class SpringApplication {
 
     private ApplicationContext doRun(Class<?> primarySource, String... args) {
         try {
-            // 1.先处理主类的注解
+            // 先检查主类的注解
             SpringBootApplication annotation = AnnotationUtils.findAnnotation(
                     primarySource, SpringBootApplication.class);
             if (annotation == null) {
@@ -30,7 +30,7 @@ public class SpringApplication {
 
             AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 
-            // 2.加载配置文件
+            // 加载配置文件
             PropertySource<?> propertySource = new ResourcePropertySource("application.properties");
             context.getEnvironment().getPropertySources().addFirst(propertySource);
             context.registerBean("propertySourcesPlaceholderConfigurer",
@@ -39,19 +39,16 @@ public class SpringApplication {
 
 
 
-            // 3.扫描包注册Bean
+            // 扫描包注册Bean
             // 包括主类所在的包和其他第三方依赖的自动配置类
             // @SpringBootApplication 注解中包含了 @EnableAutoConfiguration
             // Spring 的注解处理器在 scan 时会处理这些注解
             // 当处理到 @EnableAutoConfiguration 时，会自动调用 AutoConfigurationImportSelector
             context.scan(primarySource.getPackage().getName());
 
-            // 4.刷新上下文
+            // 刷新上下文
             // 如果不调用refresh()，Bean只是被注册了定义，但没有被实例化
             context.refresh();
-
-
-            context.register(ServerProperties.class);
 
             // 创建并配置Web服务器
             WebServer webServer = WebServerFactory.getInstance()
